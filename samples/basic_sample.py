088bbd251eba0c9645b6d41f43e8d13d804f12cf
Jimmy Lin <jimmy_lin@example.com>
2020-07-24 13:45:10 +0800

    Add some comments above the sys.path things

--- samples/basic_sample.py
+++ samples/basic_sample.py
@@ -1,29 +1,31 @@
 import sys
 import os
 import json
+"""
+In case fortiate is not installed as a package, we add it to sys.path manually here.
+If you install fortiate and added it to the $PYTHONPATH, this could be skipped.
+"""
 fortiate_path = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     'fortiate'
-)  # In case fortiate is not installed as a package.
-sys.path.insert(0, fortiate_path)
+)
+sys.path.insert(1, fortiate_path)
 from parsers import Parser  # noqa
 
 
 BASE_DIR = os.path.dirname(__file__)
-FILE_INPUT_PATH = os.path.join(BASE_DIR, 'conf', 'firewall policy.conf')
-FILE_OUTPUT_PATH = os.path.join(BASE_DIR, 'conf', 'firewall policy output.conf')
+FILE_INPUT = os.path.join(BASE_DIR, 'conf', 'firewall policy.conf')
+FILE_OUTPUT = os.path.join(BASE_DIR, 'conf', 'firewall policy output.conf')
 
 p = Parser()
-with open(file=FILE_INPUT_PATH, mode='r', encoding='utf-8') as f:
+with open(file=FILE_INPUT, mode='r', encoding='utf-8') as f:
     lines = f.readlines()
 
 formatted_lines = p.get_formatted_lines(lines=lines)
 formatted_dct = p.get_formatted_dct(formatted_lines=formatted_lines)
 
-print('------ start ------')
 print(json.dumps(obj=formatted_dct, sort_keys=True, indent=4))
-print('------ end ------')
 
 reorganize_lines = p.get_reorganize_lines(formatted_lines=formatted_lines)
-with open(file=FILE_OUTPUT_PATH, mode='w', encoding='utf-8') as file:
+with open(file=FILE_OUTPUT, mode='w', encoding='utf-8') as file:
     file.writelines('\n'.join(reorganize_lines))

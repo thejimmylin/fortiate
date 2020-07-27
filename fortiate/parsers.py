f0e2eb98954c619f678e51bac5e2548b8c84fb4a
Jimmy Lin <b00502013@gmail.com>
2020-07-27 17:39:12 +0800

    Update parsers.py

--- fortiate/parsers.py
+++ fortiate/parsers.py
@@ -147,26 +147,26 @@ class FortiConfig():
         return self._commands
 
     def init_with_lines(self, lines):
-        current_config = ''
-        current_edit = ''
-        current_set = ''
+        config_key = ''
+        edit_key = ''
+        set_key = ''
         context = {}
         for index, line in enumerate(lines):
             command = IndentedShellCommand(raw=line)
             if command.split_command[0] == 'set':
                 k, value = command.split_command[:2], command.split_command[2:]
-                current_set = ' '.join(k)
-                context[current_config][current_edit][current_set] = ' '.join(value)
+                set_key = ' '.join(k)
+                context[config_key][edit_key][set_key] = ' '.join(value)
                 continue
             if command.split_command[0] == 'edit':
-                current_edit = ' '.join(command.split_command)
-                context[current_config][current_edit] = {}
+                edit_key = ' '.join(command.split_command)
+                context[config_key][edit_key] = {}
                 continue
             if command.split_command[0] == 'config':
-                current_config = ' '.join(command.split_command)
-                context[current_config] = {}
+                config_key = ' '.join(command.split_command)
+                context[config_key] = {}
                 continue
-            if command.split_command[0] in ['next', 'end']:
+            if command.split_command[0] in ('next', 'end'):
                 continue
             raise ValueError(f'An invalid line exsits in config file, index = {index}, line = {line}.')
         self.context = context

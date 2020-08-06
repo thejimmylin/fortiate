9dfa23e3d5712bbee34f13ae18820d3be4d3fd67
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-08-06 14:04:40 +0800

    Update base.py

--- samples/base.py
+++ samples/base.py
@@ -1,16 +1,17 @@
-import os
-import sys
 """
+This file is used for importing all the foriate things, so other .py file in
+samples could import foriate things from here
+
 In case fortiate is not installed as a package and is added to the $PYTHONPATH
 we add it to sys.path manually here. If fortiate has been added to your
-$PYTHONPATH already, this could be skipped.
+$PYTHONPATH already, the sys things below could be skipped.
 """
+import os
+import sys
 fortiate_path = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
 )
 sys.path.insert(1, fortiate_path)
-"""
-The above sys things could be skipped if you add fortiate to the $PYTHONPATH.
-"""
+
 from fortiate.commands import *  # NOQA
 from fortiate.configs import *  # NOQA

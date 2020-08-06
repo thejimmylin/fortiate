428efeecba2d7862688ff0f16daa82c68602718a
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-08-06 14:47:09 +0800

    Update lab.py

--- samples/lab.py
+++ samples/lab.py
@@ -1,3 +1,6 @@
 from base import FortiSetCommand
 
-fsc = FortiSetCommand("set service HTTP HTTPS RDP 'set service tcp 8080-8080 udp 0-0'\n")
+
+fsc1 = FortiSetCommand("set service HTTP HTTPS RDP 'set service tcp 8080-8080 udp 0-0'\n")
+fsc2 = FortiSetCommand()
+fsc2.cleaned_data

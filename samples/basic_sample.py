685328ba9a75b5fd4150e98f2ddb81b6a5678b83
Jimmy Lin <b00502013@gmail.com>
2020-08-06 11:37:10 +0800

    Update basic_sample.py

--- samples/basic_sample.py
+++ samples/basic_sample.py
@@ -6,7 +6,6 @@ import json
 current_dir = os.path.dirname(__file__)
 conf_file = os.path.join(current_dir, 'conf', 'firewall_policy.conf')
 
-
 with open(file=conf_file, mode='r', encoding='utf-8') as f:
     lines = f.read().splitlines()
 

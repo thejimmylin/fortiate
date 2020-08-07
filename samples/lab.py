1329062ea438f1186604239f559a529f3d57657c
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-08-07 14:42:17 +0800

    Update lab.py

--- samples/lab.py
+++ samples/lab.py
@@ -1,7 +1,8 @@
 from base import FortiSetCommand, FortiEditCommand, FortiConfigCommand
 
 
-# test1 - build a simple config with multiple commands instance
+# test2 - build a simple config with multiple commands instance
+# instance come from list instead of string
 """
 config firewall vip
     edit "vip__jimmylin__168.100.168.11"
@@ -13,10 +14,14 @@ config firewall vip
 end
 """
 
-uuid = FortiSetCommand("set uuid 35da4ce3-9dd8-494f-8525-024e0e501211")
-extip = FortiSetCommand("set extip 168.100.168.11")
-extintf = FortiSetCommand("set extintf 'port1'")
-mappedip = FortiSetCommand("set mappedip '10.100.168.11'")
+uuid = FortiSetCommand()
+uuid.midset = ['set', 'uuid', '35da4ce3-9dd8-494f-8525-024e0e501211']
+extip = FortiSetCommand()
+extip.midset = ['set', 'extip', '168.100.168.11']
+extintf = FortiSetCommand()
+extintf.midset = ['set', 'extintf', 'port1']
+mappedip = FortiSetCommand()
+mappedip.midset = ['set', 'mappedip', '10.100.168.11']
 
 commands = [uuid, extip, extintf, mappedip]
 context = {}

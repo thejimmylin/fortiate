bcdb74e39cb1160446c7a773d6ef554a7b546b85
Jimmy Lin <b00502013@gmail.com>
2020-08-16 03:52:59 +0800

    Update log.py

--- tests/log.py
+++ tests/log.py
@@ -1,5 +1,5 @@
 import logging
 
 
-logging.basicConfig(filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='testlog.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
 logging.warning('This will get logged to a file')
